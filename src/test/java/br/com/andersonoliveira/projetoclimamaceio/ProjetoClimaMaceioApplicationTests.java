a34d78644f2e60674cc8ccdcb8d2352a1700165f
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2023-07-21 14:59:19 -0300

    first commit

---
+++ src/test/java/br/com/andersonoliveira/projetoclimamaceio/ProjetoClimaMaceioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andersonoliveira.projetoclimamaceio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoClimaMaceioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
