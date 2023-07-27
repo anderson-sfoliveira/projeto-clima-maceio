302c7202daf2c46d57a95354cbe6e642121f4f79
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2023-07-26 22:02:53 -0300

    swagger

--- src/main/java/br/com/andersonoliveira/projetoclimamaceio/config/SecurityConfig.java
+++ src/main/java/br/com/andersonoliveira/projetoclimamaceio/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package br.com.andersonoliveira.projetoclimamaceio.config;
 
 import br.com.andersonoliveira.projetoclimamaceio.jwt.JwtTokenFilter;
-import br.com.andersonoliveira.projetoclimamaceio.jwt.JwtTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,7 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final JwtTokenService jwtTokenService;
 	private final JwtTokenFilter jwtTokenFilter;
 
 	@Bean
@@ -35,6 +34,8 @@ public class SecurityConfig {
 				.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(authozireConfig -> authozireConfig
+					.requestMatchers(HttpMethod.GET, "v3/api-docs/**").permitAll()
+					.requestMatchers(HttpMethod.GET, "swagger-ui/**").permitAll()
 					.requestMatchers(HttpMethod.POST, "auth/login").permitAll()
 					.anyRequest().authenticated()
 				)
